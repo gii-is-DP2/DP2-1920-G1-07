3faa9d281bc31d5217a00ae6dfaca183eeacab2d
Amine <amichachi@alum.us.es>
2020-04-20 17:32:27 +0200

    Update ValidatorTest.java

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -39,6 +39,7 @@ class ValidatorTests {
 		ConstraintViolation<Person> violation = constraintViolations.iterator().next();
 		assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
 		assertThat(violation.getMessage()).isEqualTo("must not be empty");
+	
 	}
 
 }
