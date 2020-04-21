d7bbe3330424ff518011b612d088b0f5ac0944bd
Sergio Capitan <serpercap@us.es>
2020-04-21 10:20:24 +0200

    UITest Donations created.

---
+++ src/test/java/org/springframework/samples/petclinic/DonationUI/SeeDonationListUITest.java
@@ -0,0 +1,88 @@
+package org.springframework.samples.petclinic.DonationUI;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SeeDonationListUITest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeEach
+  public void setUp() throws Exception {
+	String pathToGeckoDriver = "C:\\Users\\Capi\\AppData\\Local\\Temp";
+	System.setProperty("webdriver.gecko.driver", pathToGeckoDriver + "\\geckodriver.exe");
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testSeeDonationListUI() throws Exception {
+    driver.get("http://localhost:8080/");
+    driver.findElement(By.xpath("//a[contains(@href, '/login')]")).click();
+    driver.findElement(By.id("password")).clear();
+	driver.findElement(By.id("password")).sendKeys("4dm1n");
+	driver.findElement(By.id("username")).clear();
+	driver.findElement(By.id("username")).sendKeys("admin1");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[4]/a/span[2]")).click();
+    driver.findElement(By.linkText("Add donation")).click();
+    driver.findElement(By.linkText("See My Donations")).click();
+    assertEquals("admin1", driver.findElement(By.xpath("//table[@id='DonationsTable']/tbody/tr/td")).getText());
+    assertEquals("admin1", driver.findElement(By.xpath("//table[@id='DonationsTable']/tbody/tr[2]/td")).getText());
+    assertEquals("admin1", driver.findElement(By.xpath("//table[@id='DonationsTable']/tbody/tr[3]/td")).getText());
+    driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a/strong")).click();
+    driver.findElement(By.xpath("//a[contains(@href, '/logout')]")).click();
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+  }
+
+  @AfterEach
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
