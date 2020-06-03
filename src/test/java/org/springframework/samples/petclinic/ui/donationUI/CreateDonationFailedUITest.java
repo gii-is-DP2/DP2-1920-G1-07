75ba56273021e222478a93d3e379d03aa232ef6b
AlvarofbUS <alvfribal@alum.us.es>
2020-06-03 18:04:52 +0200

    Añadido cambios

--- src/test/java/org/springframework/samples/petclinic/ui/donationUI/CreateDonationFailedUITest.java
+++ src/test/java/org/springframework/samples/petclinic/ui/donationUI/CreateDonationFailedUITest.java
@@ -1,13 +1,18 @@
+
 package org.springframework.samples.petclinic.ui.donationUI;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,58 +24,59 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 public class CreateDonationFailedUITest {
 
 	@LocalServerPort
-	private int port;
-	private WebDriver driver;
-	private String baseUrl;
-	private boolean acceptNextAlert = true;
-	private StringBuffer verificationErrors = new StringBuffer();
+	private int				port;
+	private WebDriver		driver;
+	private String			baseUrl;
+	private boolean			acceptNextAlert		= true;
+	private StringBuffer	verificationErrors	= new StringBuffer();
+
 
 	@BeforeEach
 	public void setUp() throws Exception {
-//		String pathToGeckoDriver = "C:\\Users\\Capi\\AppData\\Local\\Temp";
-//		System.setProperty("webdriver.gecko.driver", pathToGeckoDriver + "\\geckodriver.exe");
-		driver = new FirefoxDriver();
-		baseUrl = "https://www.google.com/";
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		System.setProperty("webdriver.gecko.driver", System.getenv("webdriver.gecko.driver"));
+		//		String pathToGeckoDriver = "C:\\Users\\alvar";
+		//		System.setProperty("webdriver.gecko.driver", pathToGeckoDriver + "\\geckodriver.exe");
+		this.driver = new FirefoxDriver();
+		this.baseUrl = "https://www.google.com/";
+		this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 	@Test
 	public void testCreateDonationFailedUI() throws Exception {
-		driver.get("http://localhost:" + this.port);
-		driver.findElement(By.xpath("//a[contains(@href, '/login')]")).click();
-		driver.findElement(By.id("password")).clear();
-		driver.findElement(By.id("password")).sendKeys("4dm1n");
-		driver.findElement(By.id("username")).clear();
-		driver.findElement(By.id("username")).sendKeys("admin1");
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
-		driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[4]/a/span[2]")).click();
-		driver.findElement(By.linkText("Add donation")).click();
-		driver.findElement(By.linkText("Add Donation")).click();
-		driver.findElement(By.id("money")).click();
-		driver.findElement(By.id("money")).clear();
-		driver.findElement(By.id("money")).sendKeys("-5");
-		new Select(driver.findElement(By.id("anonymous"))).selectByVisibleText("false");
-		driver.findElement(By.xpath("//option[@value='false']")).click();
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
-		assertEquals("The money must have a value greater than 0",
-				driver.findElement(By.xpath("//form[@id='donation']/div/div[2]/div[2]/div[2]/div/span[2]")).getText());
-		driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a/strong")).click();
-		driver.findElement(By.xpath("//a[contains(@href, '/logout')]")).click();
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.get("http://localhost:" + this.port);
+		this.driver.findElement(By.xpath("//a[contains(@href, '/login')]")).click();
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("4dm1n");
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("admin1");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[4]/a/span[2]")).click();
+		this.driver.findElement(By.linkText("Add donation")).click();
+		this.driver.findElement(By.linkText("Add Donation")).click();
+		this.driver.findElement(By.id("money")).click();
+		this.driver.findElement(By.id("money")).clear();
+		this.driver.findElement(By.id("money")).sendKeys("-5");
+		new Select(this.driver.findElement(By.id("anonymous"))).selectByVisibleText("false");
+		this.driver.findElement(By.xpath("//option[@value='false']")).click();
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Assert.assertEquals("The money must have a value greater than 0", this.driver.findElement(By.xpath("//form[@id='donation']/div/div[2]/div[2]/div[2]/div/span[2]")).getText());
+		this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a/strong")).click();
+		this.driver.findElement(By.xpath("//a[contains(@href, '/logout')]")).click();
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
 	}
 
 	@AfterEach
 	public void tearDown() throws Exception {
-		driver.quit();
-		String verificationErrorString = verificationErrors.toString();
+		this.driver.quit();
+		String verificationErrorString = this.verificationErrors.toString();
 		if (!"".equals(verificationErrorString)) {
-			fail(verificationErrorString);
+			Assert.fail(verificationErrorString);
 		}
 	}
 
-	private boolean isElementPresent(By by) {
+	private boolean isElementPresent(final By by) {
 		try {
-			driver.findElement(by);
+			this.driver.findElement(by);
 			return true;
 		} catch (NoSuchElementException e) {
 			return false;
@@ -79,7 +85,7 @@ public class CreateDonationFailedUITest {
 
 	private boolean isAlertPresent() {
 		try {
-			driver.switchTo().alert();
+			this.driver.switchTo().alert();
 			return true;
 		} catch (NoAlertPresentException e) {
 			return false;
@@ -88,16 +94,16 @@ public class CreateDonationFailedUITest {
 
 	private String closeAlertAndGetItsText() {
 		try {
-			Alert alert = driver.switchTo().alert();
+			Alert alert = this.driver.switchTo().alert();
 			String alertText = alert.getText();
-			if (acceptNextAlert) {
+			if (this.acceptNextAlert) {
 				alert.accept();
 			} else {
 				alert.dismiss();
 			}
 			return alertText;
 		} finally {
-			acceptNextAlert = true;
+			this.acceptNextAlert = true;
 		}
 	}
 }
